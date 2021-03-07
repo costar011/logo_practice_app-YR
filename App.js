578bd60dd6e0025d26d1b7b84172fb364876c62d
YERIM <dnaer456@gmail.com>
2021-03-07 19:55:50 +0900

    app store yr

--- App.js
+++ App.js
@@ -10,13 +10,23 @@ import {
 const App = () => {
   return (
     <View style={styles.container}>
-      <View style={{marginBottom: 50}}>
-        <Text style={{fontSize: 30, fontWeight: `600`, color: `#51b8c2`}}>
-          YR Practice
+      <View style={{marginBottom: 60}}>
+        <Text style={{fontSize: 35, fontWeight: `800`, color: `#3280fc`}}>
+          YR APP STORE
         </Text>
       </View>
-      <TextInput style={styles.inputstyle} placeholder={`ID`}></TextInput>
-      <TextInput style={styles.inputstyle} placeholder={`PASSWORD`}></TextInput>
+      <TextInput style={styles.inputstyle} placeholder="EMAIL" />
+      <TextInput style={styles.inputstyle} placeholder="PASSWORD" />
+
+      <View style={styles.findBox}>
+        <TouchableOpacity style={styles.btn1}>
+          <Text style={styles.btnText}>SIGN UP</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.btn2}>
+          <Text style={styles.btnText}>SIGN IN</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -28,11 +38,72 @@ const styles = StyleSheet.create({
     alignItems: `center`,
     justifyContent: `center`,
   },
+
   inputstyle: {
     width: `80%`,
     height: 40,
 
-    marginBottom: 15,
+    marginBottom: 35,
+
+    borderColor: `#888`,
+    borderBottomWidth: 1,
+    borderBottomWidth: 1,
+
+    padding: 10,
+    borderRadius: 5,
+    shadowColor: `#888`,
+
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 1,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+
+  findBox: {
+    width: `80%`,
+    padding: 3,
+
+    flexDirection: `row`,
+    justifyContent: `space-between`,
+
+    marginBottom: 100,
+    marginTop: 60,
+  },
+
+  btn1: {
+    width: `50%`,
+    height: 45,
+
+    marginRight: 5,
+    borderRadius: 5,
+
+    alignItems: `center`,
+    justifyContent: `center`,
+
+    backgroundColor: `#2241a8`,
+  },
+
+  btn2: {
+    width: `50%`,
+    height: 45,
+
+    marginLeft: 5,
+    borderRadius: 5,
+
+    alignItems: `center`,
+    justifyContent: `center`,
+
+    backgroundColor: `#180f85`,
+  },
+
+  btnText: {
+    fontSize: 25,
+
+    fontWeight: `700`,
+    color: `#fff`,
   },
 });
 
